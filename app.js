b87269610f4dcff13d04bd444b139a836dc223fa
itw <venkateshvishwas12@gmail.com>
2024-12-26 16:20:09 +0530

    first

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+mongoose.connect('mongodb://localhost:27017/myDatabase');
+
+app.listen(4000, function() {
+  console.log("Server started on port 4000");
+});
